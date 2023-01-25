9fd8771fa0ee6f37823d81444f1f68c59fe9ac2b
Celeste Fernandez Tomasincig <celesteft00@gmail.com>
2023-01-25 18:04:38 -0300

    entrega final

--- scripts/cupon.js
+++ scripts/cupon.js
@@ -5,12 +5,5 @@ btnSuccess.addEventListener("click", () => {
         width: 600,
         padding: '3em',
         color: '#716add',
-        background: '#fff url(/images/trees.png)',
-        backdrop: `
-          rgba(0,0,123,0.4)
-          url("/images/nyan-cat.gif")
-          left top
-          no-repeat
-        `
       })
 	});
